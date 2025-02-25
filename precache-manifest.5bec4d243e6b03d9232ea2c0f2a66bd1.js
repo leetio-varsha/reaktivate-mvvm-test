f4b3e14ff5999c83ce3e54a00b11b6147327f535
Yuri Varshavsky <yuri.varshavsky@leetio.org>
2025-02-25 21:06:49 +0200

    Updates

---
+++ precache-manifest.5bec4d243e6b03d9232ea2c0f2a66bd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67e08d6df0a13c38b7cc62ad87af35eb",
+    "url": "/reaktivate-mvvm-test/index.html"
+  },
+  {
+    "revision": "fb631f70c377219d677a",
+    "url": "/reaktivate-mvvm-test/static/css/main.033383cc.chunk.css"
+  },
+  {
+    "revision": "6cda57de615e8374a45d",
+    "url": "/reaktivate-mvvm-test/static/js/2.7976b5e0.chunk.js"
+  },
+  {
+    "revision": "fb631f70c377219d677a",
+    "url": "/reaktivate-mvvm-test/static/js/main.6590dc79.chunk.js"
+  },
+  {
+    "revision": "47897748959302182e38",
+    "url": "/reaktivate-mvvm-test/static/js/runtime~main.49e3fc91.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/reaktivate-mvvm-test/precache-manifest.5bec4d243e6b03d9232ea2c0f2a66bd1.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reaktivate-mvvm-test/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
